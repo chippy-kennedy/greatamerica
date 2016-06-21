69a23a5f0882429cd6999652e6d4a91ab77a8d11
Chippy <chippy@case.edu>
2016-06-21 04:52:32 -0400

    got form hooked up to POST path. Started db implementation & single response model. TODO: add db methods to add, delete, and access random

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'http://rubygems.org'
+
+
+gem "sinatra"
+gem "json"
+
+# Use ActiveRecord as the ORM
+gem "sinatra-activerecord", '~> 2.0'
+
+# Use rake to execute ActiveRecord's tasks
+gem "rake"
+
+group :production do
+	# Use Postgresql for ActiveRecord
+	gem "pg"
+end
+
+group :development, :test do
+	#Use SQLite for ActiveRecord
+	gem 'sqlite3'
+end
--- app.rb
+++ app.rb
@@ -1,15 +1,27 @@
 require 'sinatra'
+require 'sinatra/activerecord'
 require 'rubygems'
 require 'json'
 
+set :database_file, 'config/database.yml'
+
+
+
 Tilt.register Tilt::ERBTemplate, 'html.erb'
-#set :html, :layout => :layout
+
+
+class Response < ActiveRecord::Base
+	  validates :jobTitle, presence: true, uniqueness: true
+		validates :jobDescription, presence: true
+end
 
 get '/' do
 	erb :home, :format => :layout
 end
 
 post '/job' do
-	@user = params[:exampleInputName]
-	erb :job, :layout => (request.xhr? ? false : :layout) 
+	@jobTitle = "Toupe Specialist"
+	@jobDescription = "You will groom the heaad's of carrots everywhere"
+	
+	erb :job, :layout => (request.xhr? ? false : :layout)
 end
