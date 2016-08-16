b21b617ef50f72d3473a22a6762bac3fafdfc2ca
Chippy <chippy@case.edu>
2016-08-16 00:31:58 -0400

    Facebook shared linked up

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,37 @@
-$( document ).ready(function() {
+/*
+ * Function to Load Elements on Body (No partials)
+ *
+ */
+$(window).load(function() {
+	console.log("Custom JS Loaded"); 
 
-	$(function (){
-		$('#form').submit(function(e){
-			e.preventDefault();
-			
-			$('#form').validate();
-			var $inputs = $("input:first").val();
+	//Form POST Submit
+	$('#form').submit(function(e){
+		e.preventDefault();
 
-			$.ajax({
-				url:'/job',
-				type: 'POST',
-				data: $(this).serialize(),
-				dataType: 'html',
-				success: function(result) {
-					$('#form-col').hide();
-					$('#res-col').html(result);
-				}
-			});
+		$.ajax({
+			url:'/job',
+			type: 'POST',
+			data: $(this).serialize(),
+			dataType: 'html',
+			success: function(result) {
+				$('#form-col').hide();
+				$('#res-col').html(result);
+			}
 		});
 	});
 
+});
 
+//Functions for post AJAX Load
+$('#job-page').load( function(){
+	console.log("Ajax JS Loaded"); 
+	/*
+	 * Facebook Share Animation 
+	 */
+	$('.btn-share').hover(function() {
+		console.log("Facebook Hover");
+		$(this).fadeOut( 500 );
+		$('.facebook-share').fadeIn( 500 );
+	});
 });
